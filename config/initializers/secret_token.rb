33b2261660d38df8898bedd43b3ac1a75c3b843e
Sarah Mustafa <sarahmustafa@dyn-86-36-43-132.wv.qatar.cmu.edu>
2013-02-17 14:42:44 +0300

    initial commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tickety::Application.config.secret_token = '5ddc480dce4dea87053bb44ec1ff24098fbeade8f1b0d588bd32b2edbc4aeb89c7ff5d181ba573f46d8ca66e09a15d171cd13a5574708401e446365dcd7d2238'
